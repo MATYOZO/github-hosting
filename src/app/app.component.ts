ab6454d7080618c8c575fd1f9fb156890915066f
MATYOZO <matisuchy13579@gmail.com>
2023-03-17 13:37:48 +0100

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,52 @@ export class AppComponent {
     },
   ];
   increase: number = 1;
+  p1: boolean = false;
+  p2: boolean = false;
+  p3: boolean = false;
+  p4: boolean = false;
+  p5: boolean = false;
+  p6: boolean = false;
+  p7: boolean = false;
+  p8: boolean = false;
+  p9: boolean = false;
   add() {
     this.increase++;
+    this.p1 = false;
+    this.p2 = false;
+    this.p3 = false;
+    this.p4 = false;
+    this.p5 = false;
+    this.p6 = false;
+    this.p7 = false;
+    this.p8 = false;
+    this.p9 = false;
+  }
+  p1f() {
+    this.p1 = true;
+  }
+  p2f() {
+    this.p2 = true;
+  }
+  p3f() {
+    this.p3 = true;
+  }
+  p4f() {
+    this.p4 = true;
+  }
+  p5f() {
+    this.p5 = true;
+  }
+  p6f() {
+    this.p6 = true;
+  }
+  p7f() {
+    this.p7 = true;
+  }
+  p8f() {
+    this.p8 = true;
+  }
+  p9f() {
+    this.p9 = true;
   }
 }
