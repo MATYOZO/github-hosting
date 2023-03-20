15b572e143dbde00e2f3d21ff859aac135827cfb
MATYOZO <matisuchy13579@gmail.com>
2023-03-20 20:07:40 +0100

    nowy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,40 +8,51 @@ import { pytania } from './interfaces/pytania';
 })
 export class AppComponent {
   title = 'Familiada';
+  wrong = new Audio('/assets/wrong.mp3');
+  right = new Audio('/assets/right.mp3');
   @HostListener('window:keydown.1')
   p1fclick() {
+    this.right.play();
     this.p1f();
   }
   @HostListener('window:keydown.2')
   p2fclick() {
+    this.right.play();
     this.p2f();
   }
   @HostListener('window:keydown.3')
   p3fclick() {
+    this.right.play();
     this.p3f();
   }
   @HostListener('window:keydown.4')
   p4fclick() {
+    this.right.play();
     this.p4f();
   }
   @HostListener('window:keydown.5')
   p5fclick() {
+    this.right.play();
     this.p5f();
   }
   @HostListener('window:keydown.6')
   p6fclick() {
+    this.right.play();
     this.p6f();
   }
   @HostListener('window:keydown.7')
   p7fclick() {
+    this.right.play();
     this.p7f();
   }
   @HostListener('window:keydown.8')
   p8fclick() {
+    this.right.play();
     this.p8f();
   }
   @HostListener('window:keydown.9')
   p9fclick() {
+    this.right.play();
     this.p9f();
   }
   @HostListener('window:keydown.enter')
@@ -54,11 +65,13 @@ export class AppComponent {
   }
   @HostListener('window:keydown.z')
   blueteam() {
+    this.wrong.play();
     this.validblueup();
   }
   @HostListener('window:keydown.x')
   redteam() {
     this.validredup();
+    this.wrong.play();
   }
   @HostListener('window:keydown.c')
   clearf() {
@@ -94,7 +107,7 @@ export class AppComponent {
     },
     {
       id: 4,
-      p1: 'sarna 7pkt',
+      p1: 'żywica 7pkt',
       p2: 'guma 6pkt',
       p3: 'miód 5pkt',
       p4: 'syrop 4pkt',
@@ -104,7 +117,7 @@ export class AppComponent {
     },
     {
       id: 5,
-      p1: 'żywica 6pkt',
+      p1: 'sarna 6pkt',
       p2: 'zając 5pkt',
       p3: 'dzik 4pkt',
       p4: 'bocian 3pkt',
@@ -149,8 +162,98 @@ export class AppComponent {
       p4: 'bluszcz 2pkt',
       p5: 'beniaminek 1pkt',
     },
+    {
+      id: 11,
+      p1: 'szarlotka 7pkt',
+      p2: 'pączki 6pkt',
+      p3: 'sernik 5pkt',
+      p4: 'drożdżówka 4pkt',
+      p5: 'piernik 3pkt',
+      p6: 'makowiec 2pkt',
+      p7: 'keks 1pkt',
+    },
+    {
+      id: 12,
+      p1: 'spacja 5pkt',
+      p2: 'enter 4pkt',
+      p3: 'shift 3pkt',
+      p4: 'alt 2pkt',
+      p5: 'ctrl 1pkt',
+    },
+    {
+      id: 13,
+      p1: 'godzina 4pkt',
+      p2: 'lista 3pkt',
+      p3: 'robota 2pkt',
+      p4: 'myśli 1pkt',
+    },
+    {
+      id: 14,
+      p1: 'do kina 4pkt',
+      p2: 'do pubu 3pkt',
+      p3: 'do teatru 2pkt',
+      p4: 'do restauracji 1pkt',
+    },
+    {
+      id: 15,
+      p1: 'muzyczny 5pkt',
+      p2: 'taneczny 4pkt',
+      p3: 'aktorski 3pkt',
+      p4: 'plastyczny 2pkt',
+      p5: 'sportowy 1pkt',
+    },
+    {
+      id: 16,
+      p1: 'gramy w grę na telefonie 7pkt',
+      p2: 'słuchamy muzyki 6pkt',
+      p3: 'śpimy 5pkt',
+      p4: 'rozmawiamy 4pkt',
+      p5: 'wyglądamy przez okno 3pkt',
+      p6: 'czytamy 2pkt',
+      p7: 'uczymy się 1pkt',
+    },
+    {
+      id: 17,
+      p1: 'spacer 9pkt',
+      p2: 'muzyka 8pkt',
+      p3: 'zakupy 7pkt',
+      p4: 'kąpiel 6pkt',
+      p5: 'spotkanie z przyjaciółmi 5pkt',
+      p6: 'sport 4pkt',
+      p7: 'wizyta u fryzjera 3pkt',
+      p8: 'masaż 2pkt',
+      p9: 'wizyta u kosmetyczki 1pkt',
+    },
+    {
+      id: 18,
+      p1: 'spodnie 6pkt',
+      p2: 'skarpety 5pkt',
+      p3: 'spódnica 4pkt',
+      p4: 'sukienka 3pkt',
+      p5: 'sweter 2pkt',
+      p6: 'szalik 1pkt',
+    },
+    {
+      id: 19,
+      p1: 'borowiki 7pkt',
+      p2: 'podgrzybki 6pkt',
+      p3: 'kurki 5pkt',
+      p4: 'sowy 4pkt',
+      p5: 'rydze 3pkt',
+      p6: 'zielonki 2pkt',
+      p7: 'maślaki 1pkt',
+    },
+    {
+      id: 20,
+      p1: 'przechowywanie w lodówce 6pkt',
+      p2: 'mrożenie 5pkt',
+      p3: 'suszenie 4pkt',
+      p4: 'kiszenie 3pkt',
+      p5: 'marynowanie 2pkt',
+      p6: 'wędzenie 1pkt',
+    },
   ];
-  increase: number = 1;
+  increase: number = 0;
   p1: boolean = false;
   p2: boolean = false;
   p3: boolean = false;
@@ -171,7 +274,6 @@ export class AppComponent {
     this.validblue++;
   }
   validredup() {
-    console.log('yes');
     this.validred++;
   }
   back() {
